f8ffe1b129bc736f59af7fbaf7a85839939bd710
Austin W Smith <auwsmit@gmail.com>
2016-05-19 19:07:52 -0700

    added packages

---
+++ elpa/goto-chg-20131228.659/goto-chg-autoloads.el
@@ -0,0 +1,15 @@
+;;; goto-chg-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("goto-chg.el") (22334 28711 529045 400000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; goto-chg-autoloads.el ends here
---
+++ elpa/popup-20160409.2133/popup-autoloads.el
@@ -0,0 +1,15 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("popup.el") (22334 27837 663231 100000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; popup-autoloads.el ends here
