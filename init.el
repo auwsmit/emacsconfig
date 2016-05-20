964e28b7fb109361ba62016788995827cbe453a2
Austin W Smith <auwsmit@gmail.com>
2016-05-19 18:38:13 -0700

    removed remnants of old init

--- init.el
+++ init.el
@@ -1,34 +1,4 @@
 ;; Disable garbage collection to speed start-up time
 ;; see: http://tiny.cc/7wd7ay
 (let ((gc-cons-threshold most-positive-fixnum))
-  (org-babel-load-file "~/.emacs.d/config.org")
-
-  ;; ;; Setup load path
-  ;; (add-to-list 'load-path (expand-file-name "setup" user-emacs-directory))
-  ;; (require 'setup-package)
-
-  ;; ;; Setup appearance early so that if something goes
-  ;; ;; wrong with the init, Emacs is still lookin' good.
-  ;; (require 'setup-appearance)
-
-  ;; ;; Setup sensible defaults for Emacs
-  ;; ;; AKA general settings
-  ;; (require 'setup-defaults)
-
-  ;; ;; Custom key bindings + related packages
-  ;; ;; (Most mode-associated bindings are under their own setup)
-  ;; (require 'setup-keybindings)
-
-  ;; ;; Setup various packages
-  ;; (require 'setup-org)
-  ;; (require 'setup-ido)
-  ;; (require 'setup-dired)
-  ;; (require 'setup-helm)
-  ;; (require 'setup-magit)
-  ;; (require 'setup-yasnippet)
-
-  ;; ;; Setup Evil mode, along with associated packages
-  ;; ;; (This file is big and separated into functions,
-  ;; ;; I recommend HideShow for easier code overview.)
-  ;; (require 'setup-evil)
-  )
+  (org-babel-load-file "~/.emacs.d/config.org"))
