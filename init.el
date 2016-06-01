ca44fba2461e77d160ddcb3bea86b3447e998594
Austin W Smith <auwsmit@gmail.com>
2016-06-01 14:21:33 -0700

    large update
    - polished some things
    - added autoloading
    - updated some key bindings
    - added restart-emacs package
    - added emacs-sentence-navigation package
    - probably other things I forgot
    
    I'll try to do more incremental commits in the future...

--- init.el
+++ init.el
@@ -1,4 +1,1 @@
-;; Disable garbage collection to speed start-up time
-;; see: http://tiny.cc/7wd7ay
-(let ((gc-cons-threshold most-positive-fixnum))
-  (org-babel-load-file "~/.emacs.d/config.org"))
+(org-babel-load-file "~/.emacs.d/config.org")
