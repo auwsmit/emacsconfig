4b84aeacffaadea49c15d9084d3bd1b5ed817998
Austin W Smith <auwsmit@gmail.com>
2016-06-10 18:45:41 -0700

    big update

--- init.el
+++ init.el
@@ -1,1 +1,5 @@
-(org-babel-load-file "~/.emacs.d/config.org")
+;; `org-babel-load-file` increases startup time, so don't do it 
+;; To reload any config changes, delete config.el and restart emacs.
+(if (file-exists-p "~/.emacs.d/config.el")
+    (load-file "~/.emacs.d/config.el")
+    (org-babel-load-file "~/.emacs.d/config.org"))
