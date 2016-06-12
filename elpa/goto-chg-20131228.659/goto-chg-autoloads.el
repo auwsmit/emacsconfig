0aa35cfddc9dbd015c24005ae8676f558b4ee85d
Austin W Smith <auwsmit@gmail.com>
2016-06-11 18:51:24 -0700

    package update

--- elpa/goto-chg-20131228.659/goto-chg-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; goto-chg-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("goto-chg.el") (22351 10470 559680 600000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; goto-chg-autoloads.el ends here
--- elpa/ivy-hydra-20160517.1349/ivy-hydra-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; ivy-hydra-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("ivy-hydra.el") (22351 10261 319978 400000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ivy-hydra-autoloads.el ends here
