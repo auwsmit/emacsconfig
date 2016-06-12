1e6a86b88109dd8b0e4e3eceb5c377f7fe404aba
Austin W Smith <auwsmit@gmail.com>
2016-06-11 19:57:36 -0700

    updated packages

---
+++ elpa/ivy-hydra-20160517.1349/ivy-hydra-autoloads.el
@@ -0,0 +1,15 @@
+;;; ivy-hydra-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ivy-hydra.el") (22364 46701 754934 500000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ivy-hydra-autoloads.el ends here
