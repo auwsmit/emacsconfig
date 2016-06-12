e86f98ec842ddd07c10662882b5e70dab4b99847
Austin W Smith <auwsmit@gmail.com>
2016-06-11 17:59:00 -0700

    forgot some words

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; `org-babel-load-file` increases startup time, so don't do it 
+;; `org-babel-load-file` increases startup time, so only do it if necessary.
 ;; To reload any config changes, delete config.el and restart emacs.
 (if (file-exists-p "~/.emacs.d/config.el")
     (load-file "~/.emacs.d/config.el")
