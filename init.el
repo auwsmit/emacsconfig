e49079153e4fe73eca268047b3f1d3f85aaa03a2
Austin W Smith <auwsmit@gmail.com>
2017-01-17 14:49:33 -0800

    A bunch of minor stuff

--- init.el
+++ init.el
@@ -2,4 +2,5 @@
 ;; To reload any config changes, delete config.el and restart emacs.
 (if (file-exists-p "~/.emacs.d/config.el")
     (load-file "~/.emacs.d/config.el")
+    ;; else
     (org-babel-load-file "~/.emacs.d/config.org"))
