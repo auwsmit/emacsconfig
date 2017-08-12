53f0f65ab7c39c178230b27bb8a3fdbc8920cdbb
Austin W Smith <auwsmit@gmail.com>
2017-08-12 13:32:57 -0700

    updated packages

---
+++ elpa/counsel-20170809.2208/counsel-pkg.el
@@ -0,0 +1,1 @@
+(define-package "counsel" "20170809.2208" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "0debbd10bf737240b9c59c534ade50fb9bc6d3f6" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
---
+++ elpa/ivy-hydra-20170703.2350/ivy-hydra-autoloads.el
@@ -0,0 +1,15 @@
+;;; ivy-hydra-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ivy-hydra.el") (22927 25999 970502 700000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ivy-hydra-autoloads.el ends here
